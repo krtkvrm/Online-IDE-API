a36e45ec09d3e93bcf98d4481abab99d47da558b
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-02-10 00:08:12 +0530

    Added support for python3

--- routes/run.js
+++ routes/run.js
@@ -36,8 +36,41 @@ let cExec = (req, res) => {
         })
     })
 }
+let py3Exec = (req, res) => {
+    let baseDIR = req.app.get('baseDIR')
+    let fileName = uuid() + ".py"
+    let inFilePath = baseDIR + "/codes/inputf.in"
+    fs.writeFileSync(inFilePath, req.body.inputf)
+    let filePathCode = baseDIR + "/codes/" + fileName
+    fs.writeFile(filePathCode, Buffer.from(req.body.program, 'base64').toString(), (error) => {
+        if(error) throw error
+        shell.cd(baseDIR + "/codes/")
+        shell.exec('python3 '+fileName+'<inputf.in ', {silent: true}, (code, stdout, stderr) => {
+            if(code === 0)
+            {
+                if(stdout === req.body.output)
+                    res.send({
+                        "result": "AC"
+                    })
+                    else
+                    res.send({
+                        "result": "WA"
+                    })
+            }
+            else
+            {
+                res.send({
+                    "result": "ERR",
+                    "error": stderr
+                })
+            }
+        })
+    })
+}
 route.post('/', (req, res, next) => {
     if(req.body.lang === 'c')
         cExec(req, res)
+    if(req.body.lang === 'py3')
+        py3Exec(req, res)
 })
 module.exports = route
--- test/codeTests.js
+++ test/codeTests.js
@@ -51,3 +51,50 @@ describe('C Testing', () => {
       })
   })
 })
+describe('Python3 Testing', () => {
+  const testCodeAC = {
+    "program": "aSA9IGludChpbnB1dCgpKQ0KDQpwcmludChpKzEsIGVuZD0nJyk=",
+    "lang": "py3",
+    "inputf": "1",
+    "output": "2"
+  }
+  const testCodeWA = {
+    "program": "aSA9IGludChpbnB1dCgpKQ0KDQpwcmludChpKzMp",
+    "lang": "py3",
+    "inputf": "1",
+    "output": "2"
+  }
+  const testCodeERR = {
+    "program": "aSA9IGludChpbnB1dCgpKQ0KDQpwcmlhYW50KGkrNSk=",
+    "lang": "py3",
+    "inputf": "1",
+    "output": "2"
+  }
+  it('Responds with AC', done => {
+    chai.request(server)
+      .post('/run')
+      .send(testCodeAC)
+      .end((err, res) => {
+        expect(res.body).to.have.property('result').eq('AC')
+        done()
+      })
+  })
+  it('Responds with WA', done => {
+    chai.request(server)
+      .post('/run')
+      .send(testCodeWA)
+      .end((err, res) => {
+        expect(res.body).to.have.property('result').eq('WA')
+        done()
+      })
+  })
+  it('Responds with ERR', done => {
+    chai.request(server)
+      .post('/run')
+      .send(testCodeERR)
+      .end((err, res) => {
+        expect(res.body).to.have.property('result').eq('ERR')
+        done()
+      })
+  })
+})
